7e19874ec0831e8ee8560d04652cc164edbeb9d6
daniel-d7 <qcuong748@gmail.com>
2025-06-16 22:02:45 +0700

    change logging location

--- modules/logging.py
+++ modules/logging.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-def log_progress(message):
+def log_progress(log_file, message):
     time_format = "%Y-%m-%d %H:%M:%S"  # Corrected format
     now = datetime.now()
     timestamp = now.strftime(time_format)
